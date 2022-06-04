4efc7cf95a8e178c31fbda304ed5bf1b4f998ce3
cmon-theres-gotta-be-a-name-available <ben.ridgeway9@gmail.com>
2022-06-04 15:44:21 -0400

    added landing page, button, and click event handler

---
+++ assets/js/js.js
@@ -0,0 +1,19 @@
+var startButton = document.querySelector("#start-button");
+
+var buttonClickHandler = function(click) {
+    if (click) {
+      start();
+    }
+};
+
+var start = function() {
+//start timer
+//move to first question
+};
+//timeLeft = 30       amount of seconds
+//var time = setInterval(startTimer, 1000)
+
+
+
+
+  startButton.addEventListener("click", buttonClickHandler);
